bc2a0e2d4094b867a493c084943fb117307280bf
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-02-01 09:49:28 +0100

    Update main.cpp

--- Quizz/main.cpp
+++ Quizz/main.cpp
@@ -3,86 +3,89 @@
 #include<fstream>
 #include<cstdlib>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
 class Pytanie
 {
     public:
-        string tresc;
-        string a,b,c,d;
-        int nr_pytania;
-        string poprawna;
-        string odpowiedz;
-        int punkt;
+    string tresc;
+    string a,b,c,d;
+    int nr_pytania;
+    string poprawna;
+    string odpowiedz;
+    int punkt;
 
-        void wczytaj()
+    void wczytaj()
+    {
+        fstream plik;
+        plik.open("quiz.txt", ios::in);
+
+        if (plik.good()==false)
         {
-            fstream plik;
-            plik.open("quiz.txt", ios::in);
-
-            if (plik.good()==false)
+            cout << "Nie udalo sie otworzyc pliku ;(" << endl;
+            for (int i = 3; i > 0; i--) 
             {
-                cout << "Nie udalo sie otworzyc pliku ;(" << endl;
-                for (int i = 3; i < 0; i--) 
-                {
-                    cout << "Program wylaczy sie za " << i << " sekund" << endl;
-                    
-                }
-                exit(0);
-
-
+                cout << "Program wylaczy sie za " << i << " sekund" << endl;
+                
             }
-            
-            int nr_linii=(nr_pytania - 1)*6+1;
-            int aktualny_nr = 1;
-            string linia;
+            exit(0);
 
-            while (getline(plik, linia))
-            {
-                if(aktualny_nr==nr_linii) tresc = linia;
-                if(aktualny_nr==nr_linii+1) a = linia;
-                if(aktualny_nr==nr_linii+2) b = linia;
-                if(aktualny_nr==nr_linii+3) c = linia;
-                if(aktualny_nr==nr_linii+4) d = linia;
-                if(aktualny_nr==nr_linii+5) poprawna = linia;
-                    
-                aktualny_nr++;                      
-            }
-        
-            plik.close();
+
         }
+        
+        int nr_linii=(nr_pytania - 1)*6+1;
+        int aktualny_nr = 1;
+        string linia;
+       
 
-
-        void zadaj()
+        while (getline(plik, linia))
         {
-            system("CLS");
-            cout << endl << tresc << endl;
-            cout << a << endl;
-            cout << b << endl;
-            cout << c << endl;
-            cout << d << endl;
-            cout << "-------------" << endl;
-            cout << "twoja odpowiedz: ";
-            cin >> odpowiedz;
+            if(aktualny_nr==nr_linii) tresc = linia;
+            if(aktualny_nr==nr_linii+1) a = linia;
+            if(aktualny_nr==nr_linii+2) b = linia;
+            if(aktualny_nr==nr_linii+3) c = linia;
+            if(aktualny_nr==nr_linii+4) d = linia;
+            if(aktualny_nr==nr_linii+5) poprawna = linia;
+            aktualny_nr++;                      
         }
+    
+        plik.close();
+    }
 
-        void sprawdz()     
+
+    void zadaj()
+    {
+        system("CLS");
+        cout << endl << tresc << endl;
+        cout << a << endl;
+        cout << b << endl;
+        cout << c << endl;
+        cout << d << endl;
+        cout << "-------------" << endl;
+        cout << "twoja odpowiedz: ";
+        cin >> odpowiedz;
+    }
+
+    void sprawdz()     
+    {
+
+        string odp_z_duzej = odpowiedz;
+        string odp_z_malej = odpowiedz;
+        transform(odp_z_duzej.begin(), odp_z_duzej.end(), odp_z_duzej.begin(), ::toupper);
+        transform(odp_z_malej.begin(), odp_z_malej.end(), odp_z_malej.begin(), ::tolower);
+        if(odp_z_malej==poprawna || odp_z_duzej==poprawna || odpowiedz==poprawna) 
         {
-            if(odpowiedz==poprawna)
-            {
-                cout << "Gratulacje zdobywasz punkt!" << endl;
-                punkt = 1;
-            }
-            else if (odpowiedz!= poprawna)
-            {
-                punkt = 0;
-                cout << "niestety nie zdobywasz punktu :(" << endl;
-            }
-        }            
-        
-        
-
+            cout << "Gratulacje zdobywasz punkt!" << endl;
+            punkt = 1;
+        }
+        else if (odpowiedz!= poprawna)
+        {
+            punkt = 0;
+            cout << "niestety nie zdobywasz punktu :(" << endl;
+        }
+    }            
 };
 
 
@@ -90,20 +93,49 @@ class Pytanie
 int main(void)
 {
     Pytanie quiz[5];
-
-    for (int i = 0; i < 4; i++)
+    
+    int suma;
+    
+    while(true)
     {
-        quiz[i].nr_pytania = i+1;
-        quiz[i].wczytaj();
-        Sleep(2000);
+        for (int i = 0; i < 4; i++)
+        {
+            quiz[i].nr_pytania = i+1;
+            quiz[i].wczytaj();
+            Sleep(500);
 
-        quiz[i].zadaj();
-        Sleep(2000);
+            quiz[i].zadaj();
+            Sleep(500);
 
-        quiz[i].sprawdz();
-        Sleep(2000);
-    }
+            quiz[i].sprawdz();
+            Sleep(500);
 
+            suma+= quiz[i].punkt;
+        }   
+        cout << endl << endl;
+        cout << "Zdobyles " << suma << " punktow" << endl;
+        cout << "KONIEC!";
+
+        char wyb;
+        cout << endl << "Again? (y/n)";
+        cin >> wyb;
+        suma = 0;
+
+        if(wyb=='n' || wyb=='N')
+        {
+            cout << endl;
+            for (int i = 3; i > 0; i--) 
+            {
+                cout << "Program wylaczy sie za " << i << " sekund" << endl;
+                Sleep(1000);                           
+            }
+            exit(0);
+        }
+        else
+        {
+            cout << "XD";
+        }
+    } 
 
     return 0;
 }
