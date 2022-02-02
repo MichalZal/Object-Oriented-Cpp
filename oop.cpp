76ebf65909eb80fc89dad28f27b202d9bf4aa87d
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-02-02 15:56:15 +0100

    Constructor-Destructor.cpp

--- oop.cpp
+++ oop.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 class Prostokat;
  
 class Punkt
@@ -56,29 +55,20 @@ public:
 };
 
 
-
-
-
-
-
-
-
-
 void sedzia(Punkt &pkt,Prostokat &p)
 {
    p.nazwa="PODMIANA";
  
-if((pkt.x>=p.x)&&(pkt.x<=p.x+p.szerokosc)&&(pkt.y>=p.y)&&(pkt.y<=p.y+p.wysokosc))
+ if((pkt.x>=p.x)&&(pkt.x<=p.x+p.szerokosc)&&(pkt.y>=p.y)&&(pkt.y<=p.y+p.wysokosc))
  
     cout<<endl<<"Punkt "<<pkt.nazwa<<" nalezy do prostokata: "<<p.nazwa;
  
-else
+ else
  
     cout<<endl<<"Punkt "<<pkt.nazwa<<" lezy POZA prostokatem: "<<p.nazwa;
- 
- 
 }
  
+
 int main()
 {
     Punkt pkt1("A",3,17);
